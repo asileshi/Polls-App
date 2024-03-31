1038194f39182e63dae489f5795c8b6390615f34
Sileshi Abebe <abebesileshi828@gmail.com>
2024-03-31 21:26:53 +0300

    choice selection implemented

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
-from django.template import loader
-from django.http import Http404
-from django.shortcuts import get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import Question,Choice
+from django.shortcuts import get_object_or_404,render
+from django.db.models import F
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
@@ -23,4 +23,22 @@ def results(request, question_id):
     return HttpResponse(response%question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s."% question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice= question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError,Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question":question,
+                "error_message":"you did'nt select a choice"
+            },
+
+        )
+    else:
+        selected_choice.votes = F("votes")+1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
